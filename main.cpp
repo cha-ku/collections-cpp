43e6781c45d58890c9d2445d66a7e08dc5616988
Chaitanya Kukde <chaitanyakukde@gmail.com>
2019-06-02 20:22:52 +0530

    Add print and add functions for linkedList

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
 
-using namespace std;
+typedef struct ListNode
+{
+    int value;
+    ListNode* next;
+    ListNode(int v): value(v) {}
+} ListNode;
+
+void printList(ListNode* front)
+{
+    ListNode* current = front;
+    while(current != nullptr)
+    {
+        std::cout << current->value << " ";
+        current = current->next;
+    }
+}
+
+void addFront(ListNode*& front, int value)
+{
+    ListNode* temp = new ListNode(value);
+    temp->next = front;
+    front = temp;
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    ListNode* list = nullptr;
+    addFront(list, 10);
+    addFront(list, 20);
+    addFront(list, -50);
+    printList(list);
 }
